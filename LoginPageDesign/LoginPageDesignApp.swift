bd8a78c22b9b0ac87255f7e96d564d68ad338289
Andrii Malnov <andrey.malnov@gmail.com>
2021-08-04 18:45:03 +0300

    Initial Commit

---
+++ LoginPageDesign/LoginPageDesignApp.swift
@@ -0,0 +1,17 @@
+//
+//  LoginPageDesignApp.swift
+//  LoginPageDesign
+//
+//  Created by Mac on 04.08.2021.
+//
+
+import SwiftUI
+
+@main
+struct LoginPageDesignApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
